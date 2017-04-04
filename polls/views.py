beecc8317a9d3b42025350dd988c48d5d24fbfc3
Nam Nguyen <Nam Nguyen>
2017-04-04 18:24:16 +0700

    secon changed

--- polls/views.py
+++ polls/views.py
@@ -19,4 +19,16 @@ def results(requset,question_id):
     return HttpResponse(response % question_id)
 
 def vote(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+
     return HttpResponse("You're voting on question %s." % question_id)
